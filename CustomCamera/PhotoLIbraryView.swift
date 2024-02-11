d976a1bc7e45e87eec53c4af1b8b8d78c5b42e7f
Iccyan21 <iccyan0429@gmail.com>
2024-02-11 16:00:55 +0900

    共有機能完成

--- CustomCamera/PhotoLIbraryView.swift
+++ CustomCamera/PhotoLIbraryView.swift
@@ -8,23 +8,91 @@
 import SwiftUI
 
 struct PhotoLibraryView: View {
-    @ObservedObject var cameraManager: CameraManager
+    @State var selectedPhoto: UIImage
+    @State private var showShareSheet = false
     
-    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
+    @EnvironmentObject var photoManager: PhotoManager
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: columns, spacing: 10) {
-                ForEach(cameraManager.photos.indices, id: \.self) { index in
-                    Image(uiImage: cameraManager.photos[index])
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(height: 100)
-                        .clipped()
-                        .cornerRadius(5)
+        VStack{
+            HStack{
+                Spacer()
+                
+                
+                
+                Text("全ての写真")
+                    .padding()
+                
+                
+            }
+            Image(uiImage: selectedPhoto) // 選択された写真を表示
+                .resizable()
+                .scaledToFill()
+            
+            HStack{
+                
+                
+                Button(action: {
+                    self.showShareSheet = true
+                }) {
+                    Image(systemName: "square.and.arrow.up")
+                        .imageScale(.large)
+                        .padding()
+                }
+                .sheet(isPresented: $showShareSheet, content: {
+                    ActivityViewController(activityItems: [self.selectedPhoto])
+                })
+                
+                Spacer()
+                
+                // 写真を削除する機能
+                Button(action: {
+                    photoManager.deletePhoto(selectedPhoto)
+                    // 必要に応じてUIを更新する処理をここに追加
+                }) {
+                    Image(systemName: "trash.fill")
+                        .imageScale(.large)
+                        .padding()
                 }
             }
-            .padding()
         }
     }
 }
+
+
+struct PhotoLibraryView_Previews: PreviewProvider {
+    static var previews: some View {
+        let photoManager = PhotoManager() // PhotoManagerのインスタンスを作成
+        let systemImage = UIImage(systemName: "photo")!
+        PhotoLibraryView(selectedPhoto: systemImage).environmentObject(photoManager) // EnvironmentObjectとして注入
+    }
+}
+
+struct ActivityViewController: UIViewControllerRepresentable {
+    var activityItems: [Any]
+    var applicationActivities: [UIActivity]? = nil
+    
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        let controller = UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {
+    }
+}
+
+
+class PhotoManager: ObservableObject {
+    @Published var photos: [UIImage] = []
+    // 写真を削除するメゾット
+    func deletePhoto(_ photo: UIImage) {
+        if let index = photos.firstIndex(of: photo) {
+            DispatchQueue.main.async {
+                self.photos.remove(at: index)
+            }
+        }
+    }
+}
+
+
+
